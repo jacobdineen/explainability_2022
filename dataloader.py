723b32843f5ee8f6eaf354ad983bb8cfda55f2a2
jdineen123 <jd5ed@virginia.edu>
2021-03-25 09:23:21 -0700

    updates

--- dataloader.py
+++ dataloader.py
@@ -31,3 +31,35 @@ def fetch_datasets():
         datasets[k] = X_train, X_test, Y_train, Y_test
 
     return datasets
+
+
+def fetch_datasets_adult_samples():
+    """Method to systematically fetch partitioned sets of a 
+       numerous Shap/Sklearn datasets and store them in a dict
+    """
+
+    calls = {
+        "adult_1000": shap.datasets.adult(),
+        "adult_2000": shap.datasets.adult(),
+        "adult_4000": shap.datasets.adult(),
+        "adult_8000": shap.datasets.adult(),
+        "adult_16000": shap.datasets.adult(),
+        "adult_32000": shap.datasets.adult(),
+    }
+
+    datasets = {}
+    samples = 1000
+    for k, v in calls.items():
+        X = v[0][:samples]
+        Y = v[1][:samples]
+        X = X.iloc[:, :-1]  # drop country column
+        X = vectorize_categorical(X)
+        v = (X, Y)
+
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            *v, test_size=0.2, shuffle=True, random_state=0
+        )
+        datasets[k] = X_train, X_test, Y_train, Y_test
+        samples *= 2
+
+    return datasets
